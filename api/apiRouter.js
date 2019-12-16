49b0acdf7a0577967dfeae4311bcfe024fad62a9
Michael Nunes <miketnunes@gmail.com>
2019-12-16 14:06:48 -0800

    Add api router

--- api/apiRouter.js
+++ api/apiRouter.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const authRouter = require('./auth/authRouter');
+const usersRouter = require('./users/usersRouter');
 
-router.use('/', authRouter);
+router.use('/auth', authRouter);
+router.use('/users', usersRouter);
 
 module.exports = router;
