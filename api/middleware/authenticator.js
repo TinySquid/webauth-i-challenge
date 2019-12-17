17dc5cb3f8c14e66f8d63d523da735240f373e2f
Michael Nunes <miketnunes@gmail.com>
2019-12-16 17:17:41 -0800

    Add session middleware

--- api/middleware/authenticator.js
+++ api/middleware/authenticator.js
@@ -1,5 +1,9 @@
 module.exports = authenticator;
 
 function authenticator(req, res, next) {
-
+  if (req.session && req.session.userId) {
+    next();
+  } else {
+    res.status(401).json({ message: 'you shall not pass!!' });
+  }
 }
