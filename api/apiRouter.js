e08b59600a57f3b06ebcb61ba68a4d76db80630e
Michael Nunes <miketnunes@gmail.com>
2019-12-17 09:36:59 -0800

    Add stretch (restricted middleware)

--- api/apiRouter.js
+++ api/apiRouter.js
@@ -1,9 +1,17 @@
 const router = require('express').Router();
 
+const authenticator = require('./middleware/authenticator');
+
 const authRouter = require('./auth/authRouter');
 const usersRouter = require('./users/usersRouter');
+const restrictedRouter = require('./restricted/restrictedRouter');
+
+//Restrict any route based on the restricted path to allow only logged in users to access
+router.use("/restricted", authenticator);
 
 router.use('/auth', authRouter);
 router.use('/users', usersRouter);
+router.use('/restricted', restrictedRouter);
+
 
 module.exports = router;
